86d53054f8362728558d3d217e19ef7ea38db04f
Dongsheng Yang <dongsheng.yang@linux.dev>
2024-11-08 06:07:45 +0000

    cbdctrl: add libcbdsys.h and libcbdsys.c
    Signed-off-by: Dongsheng Yang <dongsheng.yang@linux.dev>

--- src/cbdctrl.h
+++ src/cbdctrl.h
@@ -1,5 +1,5 @@
-#ifndef CBDCTL_H
-#define CBDCTL_H
+#ifndef CBDCTRL_H
+#define CBDCTRL_H
 
 
 #include <stdbool.h>
@@ -14,10 +14,6 @@
 #define CBDCTL_BACKEND_STOP "backend-stop"
 #define CBDCTL_DEV_START "dev-start"
 #define CBDCTL_DEV_STOP "dev-stop"
-
-#define SYSFS_CBD_TRANSPORT_REGISTER "/sys/bus/cbd/transport_register"
-#define SYSFS_CBD_TRANSPORT_UNREGISTER "/sys/bus/cbd/transport_unregister"
-#define SYSFS_TRANSPORT_BASE_PATH "/sys/bus/cbd/devices/transport"
 
 #define CBD_BACKEND_HANDLERS_MAX 128
 
@@ -79,48 +75,4 @@ int cbdctrl_dev_start(cbd_opt_t *options);
 int cbdctrl_dev_stop(cbd_opt_t *options);
 int cbdctrl_gc(cbd_opt_t *options);
 
-static inline void transport_adm_path(int transport_id, char *buffer, size_t buffer_size)
-{
-	/* Generate the path with transport_id */
-	snprintf(buffer, buffer_size, "%s%u/adm", SYSFS_TRANSPORT_BASE_PATH, transport_id);
-}
-
-static inline void transport_hosts_dir(int transport_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_hosts/", SYSFS_TRANSPORT_BASE_PATH, transport_id);
-}
-
-static inline void host_alive_path(int transport_id, int host_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_hosts/host%u/alive", SYSFS_TRANSPORT_BASE_PATH, transport_id, host_id);
-}
-
-static inline void transport_blkdevs_dir(int transport_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_blkdevs/", SYSFS_TRANSPORT_BASE_PATH, transport_id);
-}
-
-static inline void blkdev_alive_path(int transport_id, int dev_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_blkdevs/blkdev%u/alive", SYSFS_TRANSPORT_BASE_PATH, transport_id, dev_id);
-}
-
-static inline void blkdev_backend_id_path(int transport_id, int dev_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_blkdevs/blkdev%u/backend_id", SYSFS_TRANSPORT_BASE_PATH, transport_id, dev_id);
-}
-
-static inline void transport_backends_dir(int transport_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_backends/", SYSFS_TRANSPORT_BASE_PATH, transport_id);
-}
-
-static inline void backend_alive_path(int transport_id, int backend_id, char *buffer, size_t buffer_size)
-{
-	snprintf(buffer, buffer_size, "%s%u/cbd_backends/backend%u/alive", SYSFS_TRANSPORT_BASE_PATH, transport_id, backend_id);
-}
-
-typedef int (*blkdev_cb_t)(unsigned int t_id, unsigned int blkdev_id);
-typedef int (*blkdev_filter_t)(unsigned int t_id, unsigned int blkdev_id, void *data);
-
-#endif // CBDCTL_H
+#endif // CBDCTRL_H
---
+++ src/libcbdsys.h
@@ -0,0 +1,51 @@
+#ifndef CBDSYS_H
+#define CBDSYS_H
+
+#define SYSFS_CBD_TRANSPORT_REGISTER "/sys/bus/cbd/transport_register"
+#define SYSFS_CBD_TRANSPORT_UNREGISTER "/sys/bus/cbd/transport_unregister"
+#define SYSFS_TRANSPORT_BASE_PATH "/sys/bus/cbd/devices/transport"
+
+static inline void transport_adm_path(int transport_id, char *buffer, size_t buffer_size)
+{
+	/* Generate the path with transport_id */
+	snprintf(buffer, buffer_size, "%s%u/adm", SYSFS_TRANSPORT_BASE_PATH, transport_id);
+}
+
+static inline void transport_hosts_dir(int transport_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_hosts/", SYSFS_TRANSPORT_BASE_PATH, transport_id);
+}
+
+static inline void host_alive_path(int transport_id, int host_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_hosts/host%u/alive", SYSFS_TRANSPORT_BASE_PATH, transport_id, host_id);
+}
+
+static inline void transport_blkdevs_dir(int transport_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_blkdevs/", SYSFS_TRANSPORT_BASE_PATH, transport_id);
+}
+
+static inline void blkdev_alive_path(int transport_id, int dev_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_blkdevs/blkdev%u/alive", SYSFS_TRANSPORT_BASE_PATH, transport_id, dev_id);
+}
+
+static inline void blkdev_backend_id_path(int transport_id, int dev_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_blkdevs/blkdev%u/backend_id", SYSFS_TRANSPORT_BASE_PATH, transport_id, dev_id);
+}
+
+static inline void transport_backends_dir(int transport_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_backends/", SYSFS_TRANSPORT_BASE_PATH, transport_id);
+}
+
+static inline void backend_alive_path(int transport_id, int backend_id, char *buffer, size_t buffer_size)
+{
+	snprintf(buffer, buffer_size, "%s%u/cbd_backends/backend%u/alive", SYSFS_TRANSPORT_BASE_PATH, transport_id, backend_id);
+}
+
+int backend_blkdevs_clear(unsigned int t_id, unsigned int backend_id);
+
+#endif // CBDSYS_H
